ff8cdfe0b260d7427bb70b9b2d78fb5a423365ca
Victor Vogelpoel <victor.vogelpoel@theone.nl>
2020-06-12 14:16:57 +0200

    Change blog title

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -20,7 +20,7 @@ export const config = /*#__PURE__*/configuration({
   },
   page: {
     title: {
-      base: 'Coding Blog Boilerplate'         // --> change this to change your blog's title
+      base: 'victorvogelpoel.coding.blog'         // --> change this to change your blog's title
     },
     favicon: '/favicon.ico'
   },
